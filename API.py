5842ec7a9bdbfcb693c48f3a1cccd214cbea7cbf
AdamJ2009 <adamdj2009@gmail.com>
2026-08-20 14:09:59 +0100

    made queue a delta

--- API.py
+++ API.py
@@ -52,7 +52,7 @@ def get_job_times(base_command,count):
     print(result)
     #Parse array
     diff = datetime.timedelta(seconds=0)
-    queue = 0
+    queue = datetime.timedelta(seconds=0)
     for i in range(3,len(result),3):
         start = datetime.datetime.strptime(result[i], '%Y-%m-%dT%H:%M:%S')
         end = datetime.datetime.strptime(result[i+1], '%Y-%m-%dT%H:%M:%S')
