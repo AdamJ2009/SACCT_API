03ac4798b9fe8ce5bd4f13f536589992eefe1887
AdamJ2009 <adamdj2009@gmail.com>
2026-08-20 16:19:44 +0100

    make quota executable

--- API.py
+++ API.py
@@ -143,6 +143,7 @@ def get_user_metrics(name: str):
         average_time,average_queue = get_job_times(base_command,count)
         node,cpu,tasks,nodelist = get_shape(base_command,count)
         partitions = get_partition_list(base_command)
+        diskquota(name)
         data = { "user":name,
                 "last":{
                     "access":access_str,
