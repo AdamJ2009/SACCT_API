1d7daf6c191c7424c0d0464b0f22be6ab893ddbc
AdamJ2009 <adamdj2009@gmail.com>
2026-08-24 10:32:59 +0100

    added missing colon

--- API.py
+++ API.py
@@ -125,7 +125,7 @@ def convert_mb(value):
     else:
         return value / (1024*1024)
 
-def time_converter(value)
+def time_converter(value):
     time = datetime.datetime.strptime(result[i], '%H:%M:%S')
     delta = datetime.timedelta(hours=time.hour,minutes=time.minute,seconds=time.second)
     return int(delta.total_seconds())
