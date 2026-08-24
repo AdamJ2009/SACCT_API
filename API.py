4b56e91ec33dcca97c481babe0370b55a76f2d49
AdamJ2009 <adamdj2009@gmail.com>
2026-08-24 11:52:39 +0100

    fixed bug

--- API.py
+++ API.py
@@ -143,7 +143,7 @@ def get_cpueff(base_command,count):
     print(result)
     cpueffsum = 0
     errors = 0
-    for i in range(1,len(result),3):
+    for i in range(3,len(result),3):
         try:
             cpueffsum += (time_converter(result[i]) / ((time_converter(result[i+1])) * int(result[i+2])))
             print(cpueffsum)
