6a0fb709b2c36e5e4a3e2cc600f332a1d52636ef
AdamJ2009 <adamdj2009@gmail.com>
2026-09-01 14:57:26 +0100

    removed an extra s, thats it, thats the entire commit

--- lib/sacct_api/cli.rb
+++ lib/sacct_api/cli.rb
@@ -56,7 +56,7 @@ module SacctApi
         option :json, aliases: ['-j'], type: :string, desc: 'Save json if not none'
 
         def call(**opts)
-          results = api_call(opts)
+          result = api_call(opts)
           Renderer.render(result)
           return if opts[:json].nil?
 
